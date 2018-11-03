904ac419e0b2c3789d819d438b2ddc7d24365510
Kevin Newman <Kevin@unFocus.com>
2018-11-03 02:05:23 -0400

    Remove async-serer - that was the wrong way to think about a problem

--- package.js
+++ package.js
@@ -3,13 +3,12 @@ Package.describe({
   name: 'npdev:async-proxy',
   summary: 'A set of utils to work with Meteor methods and other APIs with async/await',
   description: 'A set of utils to work with Meteor methods and other APIs with async/await',
-  version: '0.1.0',
+  version: '0.1.1',
   git: 'https://github.com/GoogleChrome/proxy-polyfill.git'
 })
 
 Package.onUse(function (api) {
   api.versionsFrom('METEOR@1.5')
   api.use('ecmascript')
-  api.mainModule('async.js', ['client'], { lazy: true })
-  // api.mainModule('async-server.js', ['server'], { lazy: true })
+  api.mainModule('async.js', ['client', 'server'], { lazy: true })
 })
